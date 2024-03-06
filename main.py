15f92dea5e0c84539cf9c9d4909dbdbdc3b9c138
atbarry <albarry465@gmail.com>
2024-03-06 12:19:15 -0500

    refactored the project structure

--- main.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from lib.twitterdata import load_data
-
-data = load_data()
-x = data.head()
-print(x)
-
-
--- src/twitterdata.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import networkx as nx
-
-def load_data():
-    data = pd.read_csv("data/Twitter_May-Aug.csv")
-    return data
