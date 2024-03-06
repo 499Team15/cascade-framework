f6cb30cbcf6c36a84d1218a91d0b00f2a5c88985
JS531654 <89528356+JS531654@users.noreply.github.com>
2024-03-06 12:44:46 -0500

    Added Node class

---
+++ cascadef/Node.py
@@ -0,0 +1,9 @@
+class Node:
+  def __init__(self, value, time_stamp):
+    self.value = value
+    self.time_stamp = time_stamp
+    
+  
+  def get_value(self):
+    return self.value
+    return self.time_stamp
