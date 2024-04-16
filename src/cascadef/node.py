f4f505a27ba4656f232d4a11735a809d46a82ff7
Blake Funderburke <blakefunderburke@us-mbp-2019-blakefunderburke.lan>
2024-04-16 16:24:15 -0400

    April 16 commits

--- src/cascadef/node.py
+++ src/cascadef/node.py
@@ -1,7 +1,9 @@
 class Node:
-  def __init__(self, value, time_stamp):
-    self.value = value
-    self.time_stamp = time_stamp
+  def __init__(self, value, time_stamp, content):
+        self.value = value
+        self.time_stamp = time_stamp
+        self.infection_status = False
+        self.content = content
   
   def get_value(self):
     return self.value
@@ -14,3 +16,9 @@ class Node:
 
   def set_value(self, value):
     self.value = value
+
+  def get_infection_status(self):
+    return self.infection_status
+  
+  def infected(self):
+    self.infection_status = True
