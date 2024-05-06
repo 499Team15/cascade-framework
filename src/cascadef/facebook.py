6121fa8352674b81749d049b4910b3de2e2f427c
John Syracuse <89528356+JS531654@users.noreply.github.com>
2024-05-06 09:46:21 -0400

    Update facebook.py

--- src/cascadef/facebook.py
+++ src/cascadef/facebook.py
@@ -1,7 +1,7 @@
 import requests
 import csv
 
-def get_page_posts(page_id, access_token):
+def get_posts(page_id, access_token):
     url = f"https://graph.facebook.com/{page_id}/posts"
     params = {
         "access_token": access_token,
@@ -21,9 +21,9 @@ def get_page_posts(page_id, access_token):
     return data.get('data', []) # Extract the 'data' field from the response
 
 file = open("Facebookdata.csv", "w", newline='', encoding='utf-8')
-page_id = "1365574063559367"
+page_id = "1365574063559367" #Replace with Page ID of page to pull data from
 access_token = "EAADZBeoXbPQoBO5vqpEyYK1RBsCHZBEi53THmPUjSf8hOczoi4yMENbZBDbqWn4p14O1s2KuIeH8xpzcnaep6oQQGUSfWtZBTwodcyEXeJ86nzOdhzUkOZAXYit6BXPWUVvQjhe1f0uQwJGaPyE7PPKZC63YtZAztmvCE6jVISWzIO4XiAMM35ZAoXPXZBgxJVANVIBXsOVDJm1zBxsZAdCQZDZD" #Replace with personal access token
-posts = get_page_posts(page_id, access_token)
+posts = get_posts(page_id, access_token)
 
 writer = csv.writer(file)
 writer.writerow(["Post ID", "Message", "Created Time"]) # Write header
